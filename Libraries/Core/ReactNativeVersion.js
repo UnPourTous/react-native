0a4609ed901e44f913369cb69ba6462c30104207
erichua <erichua@webank.com>
2019-11-27 19:26:12 +0800

    [0.51.8] Bump version numbers

--- Libraries/Core/ReactNativeVersion.js
+++ Libraries/Core/ReactNativeVersion.js
@@ -15,6 +15,6 @@
 exports.version = {
   major: 0,
   minor: 51,
-  patch: 7,
+  patch: 8,
   prerelease: null,
 };
--- React/Base/RCTVersion.h
+++ React/Base/RCTVersion.h
@@ -12,6 +12,6 @@
 #define RCT_REACT_NATIVE_VERSION @{ \
   @"major": @(0), \
   @"minor": @(51), \
-  @"patch": @(7), \
+  @"patch": @(8), \
   @"prerelease": [NSNull null], \
 }

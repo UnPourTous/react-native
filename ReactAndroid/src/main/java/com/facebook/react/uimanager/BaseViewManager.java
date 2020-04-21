7f5adace6de021dc9ac9925f55925e2333dc5c11
williamdeng <williamdeng@webank.com>
2020-04-21 18:11:31 +0800

    f2d58483c2aec689d7065eb68766a5aec7c96e97|Facebook Github Bot|Disable accessibility state changes of the focused view for Android API < 21

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/BaseViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/BaseViewManager.java
@@ -3,6 +3,7 @@
 package com.facebook.react.uimanager;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.text.TextUtils;
 import android.os.Build;
 import android.view.View;
@@ -174,7 +175,8 @@ public abstract class BaseViewManager<T extends View, C extends LayoutShadowNode
           (state.equals(STATE_CHECKED) && accessibilityState.getType(STATE_CHECKED) == ReadableType.String)) {
         updateViewContentDescription(view);
         break;
-      } else if (view.isAccessibilityFocused()) {
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+          && view.isAccessibilityFocused()) {
         // Internally Talkback ONLY uses TYPE_VIEW_CLICKED for "checked" and
         // "selected" announcements. Send a click event to make sure Talkback
         // get notified for the state changes that don't happen upon users' click.

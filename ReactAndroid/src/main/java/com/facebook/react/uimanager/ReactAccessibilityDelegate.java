11089f168e3eeae9d53b5a123456e1fdd4d3a781
williamdeng <williamdeng@webank.com>
2020-04-21 15:22:25 +0800

    7fb02bd90884f0a717e8151d4d30767fe38392c1|Facebook Github Bot|Rename onAccessibilityAction event on Android

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactAccessibilityDelegate.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactAccessibilityDelegate.java
@@ -178,7 +178,7 @@ public class ReactAccessibilityDelegate extends AccessibilityDelegateCompat {
       ReactContext reactContext = (ReactContext)host.getContext();
       reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
           host.getId(),
-          "performAction",
+          "topAccessibilityAction",
           event);
       return true;
     }

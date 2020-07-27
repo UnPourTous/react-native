28a73af388032e49b64646b791c9139075372018
williamdeng <williamdeng@webank.com>
2020-07-27 17:04:53 +0800

    [0.51.17-rc.01] Bump version numbers

--- React/Base/RCTVersion.h
+++ React/Base/RCTVersion.h
@@ -12,6 +12,6 @@
 #define RCT_REACT_NATIVE_VERSION @{ \
   @"major": @(0), \
   @"minor": @(51), \
-  @"patch": @(16), \
-  @"prerelease": [NSNull null], \
+  @"patch": @(17), \
+  @"prerelease": @"rc.01", \
 }
